d986e039d93da056874cc1349c08f5880b4ec392
Clément Caillaud <clement.caillaud97@gmail.com>
2025-12-01 17:38:17 +0100

    Day 1 - Part 1

---
+++ Day01_SecretEntrance/Part1.cs
@@ -0,0 +1,50 @@
+﻿using Common.Interfaces;
+using Common.Services;
+
+namespace Day01_SecretEntrance;
+
+public class Part1 : IPart
+{
+    private readonly string _inputPath;
+
+    public Part1(string inputPath)
+    {
+        _inputPath = inputPath;
+    }
+
+    public string Process()
+    {
+        var input = InputService.ReadAllLines(_inputPath);
+
+        var dialValue = 50;
+        var nbOfZeros = 0;
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            var directionLetter = input[i][0];
+            var distance = int.Parse(input[i].Replace("R", "").Replace("L", ""));
+
+            if (directionLetter == 'R')
+            {
+                dialValue += distance;
+                if (dialValue >= 100)
+                {
+                    int overflow = dialValue / 100;
+                    dialValue -= overflow * 100;
+                }
+            }
+            else if (directionLetter == 'L')
+            {
+                dialValue -= distance;
+                if (dialValue < 0)
+                {
+                    int overflow = dialValue / 100;
+                    dialValue -= overflow * 100;
+                }
+            }
+            if (dialValue == 0)
+                nbOfZeros++;
+        }
+        return nbOfZeros.ToString();
+    }
+}
---
+++ Day01_SecretEntrance/Program.cs
@@ -0,0 +1,6 @@
+﻿using Common.Application;
+using Common.Constants;
+using Day01_SecretEntrance;
+
+Application.ProcessPart(new Part1(InputPath.Example), true);
+Application.ProcessPart(new Part1(InputPath.Input));
