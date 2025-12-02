0aefa2d890ba1059226d72949df74a60395da0d1
Clément Caillaud <clement.caillaud97@gmail.com>
2025-12-02 08:52:32 +0100

    Day 2

---
+++ Day02_GiftShop/Part1.cs
@@ -0,0 +1,46 @@
+﻿using Common.Interfaces;
+using Common.Services;
+
+namespace Day02_GiftShop;
+
+public class Part1 : IPart
+{
+    private readonly string _inputPath;
+
+    public Part1(string inputPath)
+    {
+        _inputPath = inputPath;
+    }
+
+    public string Process()
+    {
+        var input = InputService.GetAllInOneLine(_inputPath);
+        var ranges = input.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+        long total = 0;
+
+        for (int i = 0; i < ranges.Length; i++)
+        {
+            var rangeStart = long.Parse(ranges[i].Split('-')[0]);
+            var rangeEnd = long.Parse(ranges[i].Split('-')[1]);
+
+            for (long j = rangeStart; j <= rangeEnd; j++)
+            {
+                var currentNumber = j.ToString();
+
+                if (currentNumber.Length % 2 != 0)
+                    continue;
+
+                var currentNumberFirstHalf = currentNumber[..(currentNumber.Length / 2)];
+                var currentNumberSecondHalf = currentNumber.Substring(currentNumber.Length / 2, currentNumber.Length / 2);
+
+                if (currentNumberFirstHalf == currentNumberSecondHalf)
+                {
+                    total += j;
+                }
+            }
+        }
+
+        return total.ToString();
+    }
+}
---
+++ Day02_GiftShop/Part2.cs
@@ -0,0 +1,50 @@
+﻿using Common.Interfaces;
+using Common.Services;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day02_GiftShop;
+
+public class Part2 : IPart
+{
+    private readonly string _inputPath;
+
+    public Part2(string inputPath)
+    {
+        _inputPath = inputPath;
+    }
+
+    public string Process()
+    {
+        var input = InputService.GetAllInOneLine(_inputPath);
+        var ranges = input.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+        long total = 0;
+
+        for (int i = 0; i < ranges.Length; i++)
+        {
+            var rangeStart = long.Parse(ranges[i].Split('-')[0]);
+            var rangeEnd = long.Parse(ranges[i].Split('-')[1]);
+
+            for (long j = rangeStart; j <= rangeEnd; j++)
+            {
+                var currentNumber = j.ToString();
+                var pattern = new StringBuilder();
+
+                for (int k = 0; k < currentNumber.Length / 2; k++)
+                {
+                    pattern.Append(currentNumber[k]);
+                    var regexPattern = $"({pattern})";
+                    var matches = Regex.Matches(currentNumber, regexPattern);
+                    if (matches.Sum(m => m.Length) == currentNumber.Length)
+                    {
+                        total += j;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return total.ToString();
+    }
+}
