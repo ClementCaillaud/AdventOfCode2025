c8351640e0c240110f7215b179b2a18aa771dde5
Clément CAILLAUD <ccaillaud@webnet.fr>
2025-12-05 12:41:37 +0100

    Day 5

---
+++ Day05_Cafeteria/Part1.cs
@@ -0,0 +1,44 @@
+﻿using Common.Interfaces;
+using Common.Services;
+
+namespace Day05_Cafeteria;
+
+public class Part1 : IPart
+{
+    private readonly string _inputPath;
+
+    public Part1(string inputPath)
+    {
+        _inputPath = inputPath;
+    }
+
+    public string Process()
+    {
+        var input = InputService.ReadAllLines(_inputPath);
+        var ranges = GetInput(input, 0);
+        var ids = GetInput(input, ranges.Count + 1);
+
+        var total = 0;
+
+        for (int i = 0; i < ids.Count; i++)
+        {
+            var id = long.Parse(ids[i]);
+            if (ranges.Exists(r => long.Parse(r.Split('-')[0]) <= id && long.Parse(r.Split('-')[1]) >= id))
+                total++;
+        }
+        return total.ToString();
+    }
+
+    private static List<string> GetInput(string[] fullInput, int startIndex)
+    {
+        List<string> items = [];
+        for (int i = startIndex; i < fullInput.Length; i++)
+        {
+            if (fullInput[i] == string.Empty)
+                break;
+
+            items.Add(fullInput[i]);
+        }
+        return items;
+    }
+}
---
+++ Day05_Cafeteria/Part2.cs
@@ -0,0 +1,75 @@
+﻿using Common.Interfaces;
+using Common.Services;
+
+namespace Day05_Cafeteria;
+
+public class Part2 : IPart
+{
+    private readonly string _inputPath;
+
+    public Part2(string inputPath)
+    {
+        _inputPath = inputPath;
+    }
+
+    public string Process()
+    {
+        var input = InputService.ReadAllLines(_inputPath);
+        var ranges = GetInput(input, 0);
+        var mergedRanges = GetMergedRanges(ranges);
+
+        long total = 0;
+
+        for (int i = 0; i < mergedRanges.Count; i++)
+        {
+            var subTotal = mergedRanges[i].Item2 - mergedRanges[i].Item1 + 1;
+            total += subTotal;
+        }
+        return total.ToString();
+    }
+
+    private static List<Tuple<long, long>> GetInput(string[] fullInput, int startIndex)
+    {
+        List<Tuple<long, long>> items = [];
+        for (int i = startIndex; i < fullInput.Length; i++)
+        {
+            if (fullInput[i] == string.Empty)
+                break;
+
+            items.Add(new Tuple<long, long>(
+                long.Parse(fullInput[i].Split('-')[0]),
+                long.Parse(fullInput[i].Split('-')[1])
+            ));
+        }
+        return items;
+    }
+
+    private static List<Tuple<long, long>> GetMergedRanges(List<Tuple<long, long>> ranges)
+    {
+        ranges.Sort((a, b) => a.Item1.CompareTo(b.Item1));
+
+        var merged = new List<Tuple<long, long>>();
+        long currentStart = ranges[0].Item1;
+        long currentEnd = ranges[0].Item2;
+
+        for (int i = 1; i < ranges.Count; i++)
+        {
+            var nextStart = ranges[i].Item1;
+            var nextEnd = ranges[i].Item2;
+
+            if (nextStart <= currentEnd + 1)
+            {
+                if (nextEnd > currentEnd)
+                    currentEnd = nextEnd;
+            }
+            else
+            {
+                merged.Add(new Tuple<long, long>(currentStart, currentEnd));
+                currentStart = nextStart;
+                currentEnd = nextEnd;
+            }
+        }
+        merged.Add(new Tuple<long, long>(currentStart, currentEnd));
+        return merged;
+    }
+}
