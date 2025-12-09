7dbbc9b5f0828fae78b9c773a98a3f39860433dd
Clément Caillaud <clement.caillaud97@gmail.com>
2025-12-09 09:44:19 +0100

    Day 7 - part 2

--- Day07_Laboratories/Part2.cs
+++ Day07_Laboratories/Part2.cs
@@ -15,28 +15,42 @@ public class Part2 : IPart
     public string Process()
     {
         var input = InputService.ReadAllLines(_inputPath);
-        var total = 1;
 
         List<int> pipeIndexes = [input[0].IndexOf('S')];
+        Dictionary<int, long> beams = new() { { input[0].IndexOf('S'), 1 } };
 
         for (int lineIndex = 1; lineIndex < input.Length; lineIndex++)
         {
             List<int> pipesToSplit = [];
             List<int> newPipes = [];
+            Dictionary<int, long> newBeams = [];
 
             for (int pipeIndex = 0; pipeIndex < pipeIndexes.Count; pipeIndex++)
             {
                 var colIndex = pipeIndexes[pipeIndex];
+
                 if (input[lineIndex][colIndex] == '^')
                 {
                     pipesToSplit.Add(colIndex);
                     newPipes.AddRange(colIndex - 1, colIndex + 1);
+
+                    newBeams.TryAdd(colIndex - 1, 0);
+                    newBeams[colIndex - 1] += beams[colIndex];
+                    newBeams.TryAdd(colIndex + 1, 0);
+                    newBeams[colIndex + 1] += beams[colIndex];
+                }
+                else
+                {
+                    newBeams.TryAdd(colIndex, 0);
+                    newBeams[colIndex] += beams[colIndex];
                 }
             }
             pipeIndexes.RemoveAll(i => pipesToSplit.Contains(i));
             pipeIndexes.AddRange(newPipes);
             pipeIndexes = [.. pipeIndexes.Distinct()];
+            beams = newBeams;
         }
+        long total = beams.Values.Sum();
         return total.ToString();
     }
 }
